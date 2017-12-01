136d9e72330a8c0f4fdd58a9e0f26117a0533df1
zxy01 <2326674367@qq.com>
2017-12-01 17:06:10 +0800

    公用方法

---
+++ application/api/controller/MedicalRecord.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zxy96
+ * Date: 2017/12/01
+ * Time: 13:00
+ */
--- application/api/controller/Util.php
+++ application/api/controller/Util.php
@@ -1,15 +1,23 @@
 <?php
+/**
+ * 公用函数
+ * User: zxy96
+ * Date: 2017/12/01
+ * Time: 17:03
+ */
 namespace app\api\controller;
 
 use app\api\model\User as UserModel;
 use think\Controller;
+use think\Request;
 
-/*
- * 检查用户登录状态
- * 参数：token,profile_id（可选）
- * 返回值:true（通过）、string（错误信息）
- */
-class TokenValidate{
+class Util{
+
+    /*
+     * 检查用户登录状态
+     * 参数：token,profile_id（可选）
+     * 返回值:true（通过）、string（错误信息）
+     */
     public static function token_validate($token, $profile_id = -1){
         $user = UserModel::get(['token' => $token]);
         //验证token是否存在
@@ -45,6 +53,27 @@ class TokenValidate{
                         return '子账号不存在';
                 }
             }
+        }
+    }
+
+    /**
+     * 文件上传
+     * 参数：request
+     */
+    public static function upload(Request $request)
+    {
+        // 获取表单上传文件
+        $file = $request->file('file');
+        if (empty($file)) {
+            return 0;
+        }
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+        if ($info) {
+            return $info->getRealPath();
+        } else {
+            // 上传失败获取错误信息
+            //$this->error($file->getError());
         }
     }
 }
