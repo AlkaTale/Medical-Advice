ca687e50c7cce8a5d9f874ec18ff4aa5d3eda9e3
Rewrite2017 <zxy@njust.edu.cn>
2017-12-26 23:35:09 +0800

    gitTest2

--- application/api/controller/gittest.php
+++ application/api/controller/gittest.php
@@ -6,9 +6,9 @@
  * Time: 23:33
  */
 
-1gitTest(commit1);
+1-2gitTest(commit2);
 2gitTest(commit1);
-3gitTest(commit1);
-4gitTest(commit1);
+3-2gitTest(commit2);
+4-2gitTest(commit2);
 5gitTest(commit1);
 6gitTest(commit1);
